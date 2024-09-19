9d529a764f2860dd7edcef97fb4e74c329b00e45
sourabh chati <sourabh26_chati@outlook.com>
2024-09-19 17:03:06 +0530

    Sourabh chati- Added Registration Form and Registration ViewModel.

---
+++ Registration.UI/ViewModels/Registration.cs
@@ -0,0 +1,17 @@
+﻿namespace Registration.UI.ViewModels
+{
+    public class Registration
+
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string city { get; set; }
+        public string phone { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string PostalCode { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
