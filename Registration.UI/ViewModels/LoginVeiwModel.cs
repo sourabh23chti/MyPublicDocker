fffdcb65cd236273a258d941159fb4ab528c3b8a
sourabh chati <sourabh26_chati@outlook.com>
2024-09-23 17:17:21 +0530

    Sourabh Chati- Login form with Validations

---
+++ Registration.UI/ViewModels/LoginVeiwModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Registration.UI.ViewModels
+{
+    public class LoginVeiwModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
