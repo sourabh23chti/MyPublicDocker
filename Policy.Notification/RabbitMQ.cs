b39b91da1726e0cda09be8224343f453368a6aad
sourabh chati <sourabh26_chati@outlook.com>
2024-10-03 19:34:40 +0530

    Sourabh Chati- Added RabbitMQ and Login, Registration Page.

---
+++ Policy.Notification/RabbitMQ.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Connections;
+using System.Text;
+using System.Threading.Channels;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using CsvHelper;
+
+namespace Policy.Notification
+{
+    public class RabbitMQ
+    {
+        public RabbitMQ()
+        {
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}");
+            };
+            channel.BasicConsume(queue: "hello",
+                             autoAck: true,
+                             consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+
+
+
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            const string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                             routingKey: "hello",
+                             basicProperties: null,
+                             body: body);
+            Console.WriteLine($" [x] Sent {message}");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+
+}
